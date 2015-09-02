48c063fd9309c6b33dae4dfa06197d2c9b40ddce
alagunambi <alagunambiwelkin@gmail.com>
2015-09-02 20:02:21 +0530

    Disabled amazon.com in controller and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,26 @@ class ApplicationController < ActionController::Base
       agent = Mechanize.new
       #page = agent.get('http://www.amazon.in/s/ref=nb_sb_noss_1/278-0803572-2415314?url=search-alias%3Daps&field-keywords=royal+canin')
       #page = agent.get('http://www.amazon.in/s/ref=nb_sb_noss_2/277-7862435-2981953?url=search-alias%3Daps&field-keywords=bajaj&rh=i%3Aaps%2Ck%3Abajaj')
-      page = agent.get(url)
-      iterate_products(page.search("li.s-result-item"), url)
 
-      loop do
-        if link = page.link_with(:dom_id => "pagnNextLink") # As long as there is still a nextpage link...
-          page = link.click
-        else # If no link left, then break out of loop
-          break
-        end
+      hostname = URI.parse(url).host
+
+      if hostname.match("amazon.com")
+        
+      else
+        page = agent.get(url)
 
         iterate_products(page.search("li.s-result-item"), url)
-      end
 
+        loop do
+          if link = page.link_with(:dom_id => "pagnNextLink") # As long as there is still a nextpage link...
+            page = link.click
+          else # If no link left, then break out of loop
+            break
+          end
+
+          iterate_products(page.search("li.s-result-item"), url)
+        end
+      end
     rescue => e
       puts "Pochu da..."
       puts e
